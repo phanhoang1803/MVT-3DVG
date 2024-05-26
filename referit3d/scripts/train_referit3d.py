cdd1d4a89108eeff3b20006f5979453300610594
HoangPhan <90397168+phanhoang1803@users.noreply.github.com>
2024-05-26 18:26:37 +0700

    Update train_referit3d.py

--- referit3d/scripts/train_referit3d.py
+++ referit3d/scripts/train_referit3d.py
@@ -77,15 +77,7 @@ if __name__ == '__main__':
     for cate in class_to_idx:
         class_name_list.append(cate)
 
-    # tokenizer = BertTokenizer.from_pretrained(args.bert_pretrain_path)
-    print(f"Loading BERT tokenizer from {args.bert_pretrain_path}")
-    try:
-        tokenizer = BertTokenizer.from_pretrained(args.bert_pretrain_path)
-    except UnicodeDecodeError as e:
-        print(f"Error loading tokenizer: {e}")
-        # Add more debugging info or steps to handle the error
-        raise e
-            
+    tokenizer = BertTokenizer.from_pretrained(args.bert_pretrain_path)
     class_name_tokens = tokenizer(class_name_list, return_tensors='pt', padding=True)
     for name in class_name_tokens.data:
         class_name_tokens.data[name] = class_name_tokens.data[name].cuda()
